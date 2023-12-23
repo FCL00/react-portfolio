6e977e904a70ce30b1ab7ec409e072894a99e4b5
FCL00 <flagahit00@gmail.com>
2023-12-23 19:43:15 +0800

    modify header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,26 @@ import { motion } from "framer-motion";
 
 export default function Header() {
   return (
-    <header className="bg-[#121212] p-3 shadow-xl">
-      <nav className="container mx-auto px-6 flex justify-between items-center">
+    <header className="bg-[#121212] py-4 shadow-xl fixed w-full z-10">
+      <nav className="container md:max-w-7xl px-6 mx-auto flex justify-between items-center">
         {/* left  */}
-        <div className=" text-white hover:text-emerald-400">
-          <a href="">Fernando</a>
+        <div className="text-white hover:text-emerald-400">
+          <a href="#hero" className="text-emerald-500 font-extrabold">
+            Fernando
+          </a>
         </div>
         {/* right */}
-        <div className="space-x-2">
-          <a className=" text-white hover:text-emerald-400" href="#">
+        <div className="hidden md:block space-x-2">
+          <a className="text-white hover:text-emerald-400" href="#about">
             About
           </a>
-          <a className=" text-white hover:text-emerald-400" href="#">
+          <a className="text-white hover:text-emerald-400" href="#projects">
             Projects
           </a>
-          <a className=" text-white hover:text-emerald-400" href="#">
+          <a className="text-white hover:text-emerald-400" href="#experience">
             Experience
           </a>
-          <a className=" text-white hover:text-emerald-400" href="#">
+          <a className="text-white hover:text-emerald-400" href="#contact">
             Contact
           </a>
         </div>
