ca924d00547433956aa301233ac34ed83c331b22
FCL00 <flagahit00@gmail.com>
2023-12-23 19:41:49 +0800

    add components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Introduction from "./components/Introduction";
+import Footer from "./components/Footer";
+import Project from "./components/Project";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Tools from "./components/Tools";
 
 export default function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Introduction />
+      <Project />
+      <Tools />
+      <Experience />
+      <Contact />
+      <Footer />
     </>
   );
 }
