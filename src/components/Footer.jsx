92282f48369c19540163b1cf47ef40a8901f19ae
FCL00 <flagahit00@gmail.com>
2023-12-24 11:31:35 +0800

    add contact links component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ContactLinks from "./partials/ContactLinks";
 import {
   faLinkedin,
   faSquareFacebook,
@@ -10,59 +11,29 @@ import {
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 
-// #10b981
+const icons = [
+  faLinkedin,
+  faSquareFacebook,
+  faViber,
+  faTelegram,
+  faDiscord,
+  faGithub,
+];
+
 export default function Footer() {
+  function displayIcons(item, index) {
+    return <ContactLinks key={index} icon={item} />;
+  }
+
   return (
     <footer className="bg-[#121212] text-white py-8">
       <div className="container md:max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between ">
         <div className="mb-4 md:mb-0">
           <h2 className="text-2xl text-emerald-500 font-semibold">Fernando </h2>
           <p className="text-sm">Building a better future.</p>
-          {/* <p className="text-sm">&copy; 2023 All rights reserved.</p> */}
         </div>
         <div className="flex space-x-1 items-center">
-          <a href="#" className="text-white hover:text-emerald-300">
-            <FontAwesomeIcon
-              className="h-10 w-10"
-              icon={faLinkedin}
-              style={{ color: "#4ade80" }}
-            />
-          </a>
-          <a href="#" className="text-white hover:text-emerald-300">
-            <FontAwesomeIcon
-              className="h-10 w-10"
-              icon={faSquareFacebook}
-              style={{ color: "#4ade80" }}
-            />
-          </a>
-          <a href="#" className="text-white hover:text-emerald-300">
-            <FontAwesomeIcon
-              className="h-10 w-10"
-              icon={faViber}
-              style={{ color: "#4ade80" }}
-            />
-          </a>
-          <a href="#" className="text-white hover:text-emerald-300">
-            <FontAwesomeIcon
-              className="h-10 w-10"
-              icon={faTelegram}
-              style={{ color: "#4ade80" }}
-            />
-          </a>
-          <a href="#" className="text-white hover:text-emerald-300">
-            <FontAwesomeIcon
-              className="h-10 w-10"
-              icon={faGithub}
-              style={{ color: "#4ade80" }}
-            />
-          </a>
-          <a href="#" className="text-white hover:text-emerald-300">
-            <FontAwesomeIcon
-              className="h-10 w-10"
-              icon={faDiscord}
-              style={{ color: "#4ade80" }}
-            />
-          </a>
+          {icons.map(displayIcons)}
         </div>
       </div>
     </footer>
