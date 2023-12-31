a0f88fc655e9fd4fb5007f4ea94a46e727896150
FCL00 <flagahit00@gmail.com>
2024-01-01 00:43:45 +0800

    add emailjs functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,63 @@
-/* eslint-disable react/no-unescaped-entities */
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import Lottie from "lottie-react";
+import Swal from "sweetalert2";
+import "@sweetalert2/theme-dark/dark.css";
 
+// components
 import ContactAnimation from "src/assets/animations/ContactAnimation.json";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_pzo4tc8",
+        "template_vddmuwb",
+        form.current,
+        "WZ6gwj45YSNkDLQ-V"
+      )
+      .then(
+        (result) => {
+          // console.log(result.text);
+
+          Swal.fire({
+            icon: "success",
+            title: "Message sent successfully!",
+            text: "I will get back to you as soon as possible.",
+          });
+
+          e.target.reset();
+        },
+        (error) => {
+          // console.log(error.text);
+
+          Swal.fire({
+            icon: "error",
+            title: "Error!",
+            text: "Failed to send message. Please try again.",
+          });
+
+          e.target.reset();
+        }
+      );
+  };
+
   return (
     <section id="contact" className="bg-[#181818]">
       <div className="container md:max-w-7xl mx-auto px-6 py-16 flex flex-col-reverse md:flex-row text-start md:py-[68px] space-y-3">
         {/* left */}
         <div className="w-full md:w-1/2">
-          <form className="container md:max-w-md bg-[#202020] p-6 rounded-lg">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="container md:max-w-md bg-[#202020] p-6 rounded-lg"
+          >
             <h1 className="text-emerald-500 text-3xl  md:text-5xl font-bold py-4">
               Contact Me
             </h1>
@@ -33,7 +78,7 @@ export default function Contact() {
               <input
                 type="text"
                 id="name"
-                name="name"
+                name="user_name"
                 className="w-full p-2 border border-none bg-[#121212] rounded-md focus:outline-none  focus:border-emerald-400 focus:border-2 outline-none text-white"
                 required
                 autoComplete="false"
@@ -49,7 +94,7 @@ export default function Contact() {
               <input
                 type="email"
                 id="email"
-                name="email"
+                name="user_email"
                 className="w-full p-2 border border-none bg-[#121212] rounded-md focus:outline-none  focus:border-emerald-400 focus:border-2 outline-none text-white"
                 required
                 autoComplete="false"
