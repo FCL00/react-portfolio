24bcf259be87a52e5aa1f5822a67c0e62f13286f
FCL00 <flagahit00@gmail.com>
2024-01-09 16:24:19 +0800

    add notes

--- src/data/keys/key.js
+++ src/data/keys/key.js
@@ -3,4 +3,5 @@ const SERVICE_ID = "service_pzo4tc8";
 const TEMPLATE_ID = "template_vddmuwb";
 const PUBLIC_KEY = "WZ6gwj45YSNkDLQ-V";
 
+// note: if you are reading this create an account on Emailjs and copy your own api keys
 export { SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY };
