13e1d403582487cbcc3457ad952f94b61a94f28d
Fernando Lagahit <flagahit00@gmail.com>
2024-11-02 00:10:45 +0800

    Update key.js

--- src/data/keys/key.js
+++ src/data/keys/key.js
@@ -1,7 +1,7 @@
 // Emailjs Keys
 const SERVICE_ID = "service_pzo4tc8";
 const TEMPLATE_ID = "template_vddmuwb";
-const PUBLIC_KEY = "WZ6gwj45YSNkDLQ-V";
+const PUBLIC_KEY = "dgBRaUMxfJCrQkyGG";
 
 // note: if you are reading this create an account on Emailjs and copy your own api keys
 export { SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY };
