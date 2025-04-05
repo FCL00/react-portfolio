64b810c2ff8701e9eb2f81dd2e5ea51b8e98b849
FCL00 <flagahit00@gmail.com>
2025-04-05 10:00:00 +0800

    chore: add image assets from Figma for hero section and UI design

---
+++ src/assets/backgrounds/index.js
@@ -0,0 +1,6 @@
+import terminal from "./terminal.png";
+import terminal1 from "./terminal-1.png"
+import control from "./controls.png";
+import iconPlus from "./icon-plus.png";
+
+export { terminal, terminal1, control, iconPlus };
