f8c2490a2048292c1d9b0cbc81141cad1d6d228b
FCL00 <flagahit00@gmail.com>
2025-04-05 10:00:00 +0800

    chore: move icon.js from src/assets to src/assets/icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,60 @@
+import aws from "@/assets/icons/aws.webp";
+import blender from "@/assets/icons/blender.webp";
+import bootstrap from "@/assets/icons/bootstrap.webp";
+import codeigniter from "@/assets/icons/codeigniter.webp";
+import csharp from "@/assets/icons/css.webp";
+import docker from "@/assets/icons/docker.webp";
+import figma from "@/assets/icons/figma.webp";
+import git from "@/assets/icons/git.webp";
+import html from "@/assets/icons/html.webp";
+import java from "@/assets/icons/java.webp";
+import javascript from "@/assets/icons/javascript.webp";
+import jquery from "@/assets/icons/jquery.webp";
+import laravel from "@/assets/icons/laravel.webp";
+import mongodb from "@/assets/icons/mongodb.webp";
+import motion from "@/assets/icons/motion.webp";
+import mysql from "@/assets/icons/mysql.webp";
+import nodejs from "@/assets/icons/nodejs.webp";
+import php from "@/assets/icons/php.webp";
+import python from "@/assets/icons/python.webp";
+import react from "@/assets/icons/react.webp";
+import redux from "@/assets/icons/redux.webp";
+import sass from "@/assets/icons/sass.webp";
+import tailwind from "@/assets/icons/tailwind.webp";
+import typescript from "@/assets/icons/typescript.webp";
+import vue from "@/assets/icons/vue.webp";
+import xd from "@/assets/icons/xd.webp";
+import github from "@/assets/icons/github.png";
+
+
+const Icons = [
+  aws,
+  blender,
+  bootstrap,
+  codeigniter,
+  csharp,
+  docker,
+  figma,
+  motion,
+  git,
+  html,
+  java,
+  javascript,
+  jquery,
+  laravel,
+  mongodb,
+  mysql,
+  nodejs,
+  php,
+  python,
+  react,
+  redux,
+  sass,
+  tailwind,
+  typescript,
+  vue,
+  xd,
+  github,
+];
+
+export default Icons;
