56c5b4c2378c8187b2df182db7bf7985782d4912
FCL00 <flagahit00@gmail.com>
2025-04-06 10:00:00 +0800

    fix: secure EmailJS keys by using environment variables

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+const SERVICE_ID = import.meta.env.SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.PUBLIC_KEY;
+
+export { SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY};
