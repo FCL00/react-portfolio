b57791b67d26ea31850f545b63f757ca7e6333f5
FCL00 <flagahit00@gmail.com>
2025-04-07 10:00:00 +0800

    chore: add backgroudImage for gradient effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'metal-gradient': `linear-gradient(to right, 
+          #343434 0%, 
+          #474747 20%, 
+          #828282 50%, 
+          #474747 75%, 
+          #343434 100%)`,
+      },
+    },
   },
   plugins: [],
 };
